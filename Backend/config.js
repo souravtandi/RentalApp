ea67ecb2eea78cd74e7cb787f50d777531e18a86
Sourav <Sourav@gmail.com>
2022-10-23 19:58:20 +0530

    prod deployment

--- Backend/config.js
+++ Backend/config.js
@@ -1,5 +1,4 @@
 module.exports = {
     MONGO_DB: "mongodb+srv://rentapp:0OuI25XJt1sClpO6@cluster0.orgh0.mongodb.net/rentalapp?retryWrites=true&w=majority",
-    //MONGO_DB: "mongodb://127.0.0.1:27017/rentalapp",
     JWT_SECRET: "CBDHBCIECOIN@#$%^)(!@#&^%%%SBHHSAJOK"
 };
